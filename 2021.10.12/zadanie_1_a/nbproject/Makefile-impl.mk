e9ee02dd24aa35497b6311789019f6aeb46c893c
W-Sikora <you@example.com>
2021-10-26 10:45:36 +0200

    init 21.10.26

--- 2021.10.12/zadanie_1_a/nbproject/Makefile-impl.mk
+++ 2021.10.12/zadanie_1_a/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=zadanie1_a.X
+PROJECTNAME=zadanie_1_a
 
 # Active Configuration
 DEFAULTCONF=default
---
+++ 2021.10.26/readme.md
@@ -0,0 +1,10 @@
+# 2021.10.26
+
+## Info
+
+  
+## Zadanie do realizacji (termin: 21.11.02)
+
+
+## Wskazówki
+
